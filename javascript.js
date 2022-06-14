8d3f0e826af0316f0bbd259097b8db53ea979943
SlothSan <mikeoram01@gmail.com>
2022-06-14 16:21:48 +0100

    Added while loop to check the user input against the array of acceptable plays.
    Changed for loop to use a boolean that is set to False when either Player or Computer reaches a score of 5.

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ if scissors versus paper - scissors win
 if scissors versus rock - rock wins
 */
 const gameArray = [`Rock`, `Paper`, `Scissors`];
+let round = true;
 let message = ``;
 let playerScore = 0;
 let computerScore = 0;
@@ -31,7 +32,6 @@ function computerPlay () {
 function caseNormalize (string) { 
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
-
 
 //Function to play one round and return a message to the console.
 function playRound (computerSelection, playerSelection) {
@@ -83,21 +83,37 @@ let computerSelection = computerPlay();
 
 function game () {
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Enter your choice!(Rock, Paper or Scissors)")
-        playerSelection = caseNormalize(playerSelection);
+    for (;round;) {
+        
+            playerSelection = prompt("Enter your choice!(Rock, Paper or Scissors)")
+            playerSelection = caseNormalize(playerSelection);
+
+        //While to check the input matches the list of playable strings.   
+        while (!gameArray.includes(playerSelection)) {
+            alert("Please enter your choice again, you didn't pick Rock, Paper or Scissors!");
+            playerSelection = prompt("Enter your choice!(Rock, Paper or Scissors)")
+            playerSelection = caseNormalize(playerSelection);
+        }
+
         let computerSelection = computerPlay();
         console.log(playRound(computerSelection,playerSelection));
         console.log(playerScore,computerScore)
+
+
+        //sets round to true that ends the game and exits the for loop. 
+        if (playerScore === 5 || computerScore === 5) {
+            round = false;
+        } 
     }
 
     if (playerScore > computerScore) {
-        console.log(`Player Wins!`)
+        console.log(`Player Wins! Good Job!`);
     } else if (playerScore < computerScore) {
-        console.log(`Computer Wins!`)
-    } else {
-        console.log(`It's a Draw!`)
+        console.log(`Computer Wins! Skynet is taking over!`);
     }
+
+
+
 }
 
 game();
