e86fe3da68020eafe823bde1f0647959ea71c098
SlothSan <mikeoram01@gmail.com>
2022-06-15 20:50:05 +0100

    Refactoring complete to add a reset button and re-do all functions.
    Styling to be done.

--- javascript2.js
+++ javascript2.js
@@ -13,6 +13,19 @@ const COMPUTERCHOICES = [
     {choice: `Scissors`, value: 2}
 ];
 
+const WINNERRESULTS = {
+    computer: [`You Lost the game to a computer, skynet is real huh?`, `red`],
+    player: [`You Won, Grats!!`, `green`],
+    tie: [`The Game is a Tie!, good match!`, `blue`]
+}
+
+//Refresh page for new game.
+resetButton.addEventListener(`click`, () => location.reload());
+
+//Setup Player Choice Button Event Listeners 
+
+optionsButtons.forEach(button => {button.addEventListener(`click`, getPlayerChoice) });
+
 let playerCurrentScore = 0;
 let computerCurrentScore = 0;
 let playerChoice;
@@ -37,18 +50,39 @@ function playRound (playerSelection, computerSelection) {
         roundResults.textContent = `You Win! ${playerChoice} beats ${computerSelection.choice}!`;
     } else {
         computerScore.textContent = ++computerCurrentScore;
-        roundResults.textContent = ++`You Lose! ${computerSelection.choice} beats ${playerChoice}!`;
+        roundResults.textContent = `You Lose! ${computerSelection.choice} beats ${playerChoice}!`;
     }
+  checkWinner();
 }
 
 
+//Function to check the winner.
+function checkWinner () {
+    if (computerCurrentScore === 5 || playerCurrentScore === 5) {
+        if (computerCurrentScore === playerCurrentScore) {
+            updateWinner(`tie`);
+        } else {
+            let win = `${(computerCurrentScore > playerCurrentScore) ? `computer` : `player`}`;
+            updateWinner(win);
+        }
+    }
+}
 
-optionsButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        alert("Hello World!");
+//Function to update the winner.
+function updateWinner (winner) {
+    roundResults.textContent = WINNERRESULTS[winner][0];
+    roundResults.style.color = WINNERRESULTS[winner][1];
+
+    optionsButtons.forEach(button => {
+        button.removeEventListener(`click`, getPlayerChoice);
     });
-});
+}
 
-resetButton.addEventListener('click', () => {
-    alert("Hello Reset!");
-});
+//Function to get playerchoice from button click.
+function getPlayerChoice (e) {
+    let playerSelection = (e.target.id);
+    playerChoice = e.target.textContent;
+    playRound(playerSelection, computerPlay());
+}
+
+
